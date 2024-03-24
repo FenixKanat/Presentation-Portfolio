c0b78c6fba6b7ac9e55c246701780389e56a86dd
FenixKanat <fenix.k.97@outlook.com>
2024-03-24 22:03:40 +0100

    adding dependencies etc.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest:'uploads/'});
-
+const aws = require('aws-sdk');
 const app = express();
+
+const upload = multer({
+    
+    limits: {fileSize :1000000 },
+    dest:'uploads/'
+    
+});
+
 
 app.use(express.static('public'))
 
